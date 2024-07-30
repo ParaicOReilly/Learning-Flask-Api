8d3bfabc691d45f46ca6a50569889dc3963d3a2f
paraicoreilly <poreill1@tcd.ie>
2024-07-30 20:53:03 +0100

    Quick hello world endpoint to test requests

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 # wrapping app in an api - initialising a restful api
 api = Api(app)
 
+# Example: creating a resource
+class HelloWorld(Resource):
+    def get(self):
+        return {"data":"Hello World"}
+
+api.add_resource(HelloWorld, "/helloworld")
+
 if __name__ == "__main__":
     app.run(debug=True)
