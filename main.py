54a178b764787f7e326050128dafd147324ffe34
paraicoreilly <poreill1@tcd.ie>
2024-08-05 21:34:54 +0100

    Replaced the dictionary which was used to store information with a database using sql alchemy. Updated get and put methods accordingly.

--- main.py
+++ main.py
@@ -1,51 +1,72 @@
 from flask import Flask
-from flask_restful import Api, Resource,reqparse, abort
+from flask_restful import Api, Resource,reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # wrapping app in an api - initialising a restful api
 api = Api(app)
+# defining the location of the database - 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# wrapping app in database
+db = SQLAlchemy(app)
 
-# # Example: creating a resource
-# class HelloWorld(Resource):
-#     def get(self):
-#         return {"data":"Hello World"}
-# api.add_resource(HelloWorld, "/helloworld")
 
-videos = {}
+# Need to create a Resource for storing videos
+
+class Video_Model(db.Model):
+    video_id = db.Column(db.Integer, primary_key=True)
+    video_name = db.Column(db.String(100), nullable=False)
+    video_likes = db.Column(db.Integer, nullable=False)
+    video_views = db.Column(db.Integer, nullable = False)
+
+    def __repr__(self):
+        return f"Video(video_name = {self.video_name}, video_likes = {self.video_likes}, video_views = {self.video_views})"
+
+
+# Don't run this multiple times 
+# with app.app_context():
+#     db.create_all()
 
 # argument parser 
 # these arguments if these are not in the specified request where this is called it sends an error automatically
 # required = True makes it throw an error if arg is missing/wrong
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video", required = True)
-video_put_args.add_argument("views", type=int, help="Views on the video", required = True)
-video_put_args.add_argument("likes", type=int, help="Likes on the video", required = True)
+video_put_args.add_argument("video_name", type=str, help="Name of the video", required = True)
+video_put_args.add_argument("video_views", type=int, help="Views on the video", required = True)
+video_put_args.add_argument("video_likes", type=int, help="Likes on the video", required = True)
 
-def abort_if_no_id(video_id):
-    if video_id not in videos:
-        abort(404, message="Video not found")
-
-def abort_if_id_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video id already exists")
-
+# call with marshall with and will serialise what the query returns in this JSON format
+resource_fields = {
+    'video_id' : fields.Integer,
+    'video_name' : fields.String,
+    'video_likes' : fields.Integer,
+    'video_views' : fields.Integer
+}
 class Video(Resource):
     # returns information stored in dict for the video id
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_no_id(video_id)
-        return videos[video_id]
+       res = Video_Model.query.get(video_id)
+       if not res:
+           abort(409, "Video not found.")
+       return res
     
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_id_exists(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        res = Video_Model.query.get(video_id)
+        if res:
+            abort(409, message = "Video ID is in use.")
+        video =  Video_Model(video_id = video_id, video_name = args['video_name'], video_likes = args['video_likes'], video_views = args['video_views'] )
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
     
-    def delete(self,video_id):
-        abort_if_no_id(video_id)
-        if video_id in videos:
-            del(videos[video_id])
-            return 'video deleted', 204
+    # def delete(self,video_id):
+    #     abort_if_no_id(video_id)
+    #     if video_id in videos:
+    #         del(videos[video_id])
+    #         return 'video deleted', 204
         
 #<> lets you specify types 
 api.add_resource(Video, "/video/<int:video_id>")
--- test.py
+++ test.py
@@ -2,23 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-headers = {"Content-Type": "application/json; charset=utf-8"}
 
 
-data = [{"name":"Suits clips", "likes":10, "views":1000000},
-        {"name":"Cooking videos", "likes":10, "views":10000000},
-        {"name":"Coding tutorials", "likes":10, "views":1}]
+
+data = [{"video_name":"Suits clips", "video_likes":10, "video_views":1000000},
+        {"video_name":"Cooking videos", "video_likes":10, "video_views":10000000},
+        {"video_name":"Coding tutorials", "video_likes":10, "video_views":1}]
 
 for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), json = data[i], headers=headers)
-    print(response.json())
+    response = requests.put(BASE + "video/" + str(i), json = data[i])
+    print(response)
 
 input()
 response = requests.get(BASE + "video/1")
 print(response.json())
-input()
-response = requests.delete(BASE + "video/1")
-print(response)
-input()
-response = requests.get(BASE + "video/1")
-print(response)
